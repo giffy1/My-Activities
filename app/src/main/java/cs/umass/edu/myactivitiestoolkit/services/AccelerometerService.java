5b4ae272b1b735b97037cf0f3ddf4d4d8652bee3
Gahyun (Susie) Kim <gahyunkim@umass.edu>
2016-09-14 18:09:47 -0400

    Registered accelerometer

--- app/src/main/java/cs/umass/edu/myactivitiestoolkit/services/AccelerometerService.java
+++ app/src/main/java/cs/umass/edu/myactivitiestoolkit/services/AccelerometerService.java
@@ -152,12 +152,12 @@ public class AccelerometerService extends SensorService implements SensorEventLi
      */
     @Override
     protected void registerSensors(){
-
+        Log.i(TAG, "Register");
         //TODO : (Assignment 0) Register the accelerometer sensor from the sensor manager.
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        
-
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometerSensor =  mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        Log.i(TAG, "Finish registering");
         //TODO : (Assignment 1) Register your step detector. Register an OnStepListener to receive step events
     }
 
@@ -167,6 +167,7 @@ public class AccelerometerService extends SensorService implements SensorEventLi
     @Override
     protected void unregisterSensors() {
         //TODO : Unregister your sensors. Make sure mSensorManager is not null before calling its unregisterListener method.
+        mSensorManager.unregisterListener(this, mAccelerometerSensor);
     }
 
     @Override
@@ -213,12 +214,15 @@ public class AccelerometerService extends SensorService implements SensorEventLi
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        Log.d(TAG, "SENSOR RECEIVED");
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
 
             // convert the timestamp to milliseconds (note this is not in Unix time)
             long timestamp_in_milliseconds = (long) ((double) event.timestamp / Constants.TIMESTAMPS.NANOSECONDS_PER_MILLISECOND);
 
             //TODO: Send the accelerometer reading to the server
+            Log.d(TAG, "X : " + event.values[0] + ", Y : " + event.values[1] + ", Z : " + event.values[2]);
+
 
             //TODO: broadcast the accelerometer reading to the UI
 
