c7d9f8aa883960b05b30447f818ddc9bb1b007e7
giffy1 <giffy1991@gmail.com>
2016-12-27 07:12:50 +0000

    Removed inferred types (Java 8+) from instantiations.

--- app/src/main/java/cs/umass/edu/myactivitiestoolkit/clustering/Cluster.java
+++ app/src/main/java/cs/umass/edu/myactivitiestoolkit/clustering/Cluster.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class Cluster<T extends Clusterable<T>> {
     private ArrayList<T> points;
     public Cluster(){
-        points = new ArrayList<T>();
+        points = new ArrayList<>();
     }
 
     /**
