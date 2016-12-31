2f651392dc6213e99fc6a3b15b8e0df54af971e6
giffy1 <giffy1991@gmail.com>
2016-12-31 11:10:53 +0100

    Made data collection standalone script. User need only change client user ID and the activities, then run the script with params <activity> <trial#>

--- python/A2/collect_labelled_activity_data.py
+++ python/A2/collect_labelled_activity_data.py
@@ -13,18 +13,10 @@ the server and saves it in .csv format to disk.
 
 import numpy as np
 import os
+from ...client import Client
 
-# TODO: Change the filename of the output file.
-# You should keep it in the format "speaker-data-<activity>-#.csv"
-filename="activity-data-walking-1.csv" #"activity-data-walking-1.csv"
-
-# TODO: Change the label to match the activity; it must be numeric
-label = 2
-
-data_dir = "data"
-
-if not os.path.exists(data_dir):
-    os.mkdir(data_dir)
+# TODO: Change this to match your activities
+activities = ["walking", "running"]
 
 labelled_data = []
 
@@ -47,3 +39,27 @@ def save_data_to_disk():
     global labelled_data
     with open(os.path.join(data_dir, filename), "wb") as f:
         np.savetxt(f, labelled_data, delimiter=",")
+        
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: %s <activity> <index, default=1>" % sys.argv[0])
+    else:
+        if len(sys.argv) == 2:
+            index=1
+        else:
+            index = sys.argv[2]
+        activity = sys.argv[1]
+        label = activities.index(activity)
+        data_dir = "data"
+        filename="activity-data-" + str(activity) + "-" + str(index) + ".csv"
+
+        if not os.path.exists(data_dir):
+            os.mkdir(data_dir)
+
+        c = Client("") # TODO: Pass in user ID
+        # To collect activity data, uncomment just these next two lines:
+        c.map_data_to_function("SENSOR_ACCEL", collect_labelled_activity_data.write_data)
+        c.set_disconnect_callback(collect_labelled_activity_data.save_data_to_disk)
+        
+        # connect to the server to begin:
+        c.connect()
